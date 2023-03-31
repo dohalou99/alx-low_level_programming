7301dca3c5cf803e314212e9529e2ac2a4a8ac77
dohalou99 <dohaelouakili99@gmail.com>
2023-03-30 17:01:31 -0700

    arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* reverse_array - a function that reverses the content of an array of integers.
+* @a: array
+* @n: number of elements of array
+*
+* Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+	int l;
+
+	for (i = 0; i < n--; i++)
+	{
+	l = a[i];
+	a[i] = a[n];
+	a[n] = l;
+	}
+}
+
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*@n: pointer
+*
+* Return: n
+*/
+char *string_toupper(char *n)
+{
+	int j;
+
+	j = 0;
+	while (n[j] != '\0')
+	{
+	if (n[j] >= 'a' && n[j] <= 'z')
+	n[j] = n[j] - 32;
+	j++;
+	}
+	return (n);
+}
