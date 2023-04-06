327ecdda0df1575514184c98a0eabb9c12cb93f3
dohalou99 <dohaelouakili99@gmail.com>
2023-04-06 14:04:19 -0700

    it so important to dream

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
-  *_puts_recursion - Write a function that prints a string
-  *
-  *@s : the input string
-  *
-  *return : Always 0 (success)
-  */
-
+ * _puts_recursion - Write a function that prints a string
+ * @s: the input string
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s ==  0)
 	{
 		_putchar('\n');
 		return;
+
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,14 @@
-#include"main.h"
-/**
- *_print_rev_recursion -  Write a function that prints a string in reverse
- *@s : the input string
- *
- *return : always 0 (success)
- */
+#include "main.h"
 
+/**
+ * _print_rev_recursion - Write a function that prints a string in reverse
+ * @s: The printed string
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s);
-		s--;
 	}
 }
