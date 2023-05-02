b1be259dcba91a326d692759fe1dbbe1d7a7dadc
dohalou99 <dohaelouakili99@gmail.com>
2023-05-02 14:28:18 -0700

    love ur self first

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *free_listint - Write a function that frees a listint_t list.
+ *@head: the input pointer
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *newnode;
+
+	while (head)
+	{
+	newnode = head->next;
+	free(head);
+	head = newnode;
+	}
+}
