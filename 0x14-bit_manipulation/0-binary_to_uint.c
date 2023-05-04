2c7794e9eace78ab8fb4a2b598c8ceff7e1a3281
dohalou99 <dohaelouakili99@gmail.com>
2023-05-04 16:07:30 -0700

    be proud

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int j;
+	int x;
 	unsigned int val = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (j = 0; b[j]; j++)
+	for (x = 0; b[x]; x++)
 	{
-		if (b[j] < '0' || b[j] > '1')
+		if (b[x] < '0' || b[x] > '1')
 			return (0);
-		val = 2 * val + (b[j] - '0');
+		val = 2 * val + (b[x] - '0');
 	}
 	return (val);
 }
