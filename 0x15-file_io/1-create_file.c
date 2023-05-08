0bb2fafba34e98e24ae64064b3cc796967d65e1a
dohalou99 <dohaelouakili99@gmail.com>
2023-05-08 14:15:54 -0700

    be fine

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int lee, w, crea;
 
+	if (filename == NULL)
+		return (-1);
+
 	if (text_content == NULL)
 	{
 		for (crea = 0; text_content[crea];)
 			crea++;
 	}
-	if (filename == NULL)
-		return (-1);
 
 	lee = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(lee, text_content, crea);
