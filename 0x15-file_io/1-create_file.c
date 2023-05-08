364e3a82510c98697d24799f439baded1393a002
dohalou99 <dohaelouakili99@gmail.com>
2023-05-08 14:23:46 -0700

    be fine

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,21 +11,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int lee, w, crea;
+	int lee, y, crea = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (crea = 0; text_content[crea];)
 			crea++;
 	}
 
 	lee = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	w = write(lee, text_content, crea);
+	y = write(lee, text_content, crea);
 
-	if (lee == -1 || w == -1)
+	if (lee == -1 || y == -1)
 		return (-1);
 
 	close(lee);
