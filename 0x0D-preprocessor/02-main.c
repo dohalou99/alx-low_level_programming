739966fcfa7a137f26345b9cae7335d19628d034
dohalou99 <dohaelouakili99@gmail.com>
2023-06-19 13:25:15 -0700

    have a good day

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the basic file
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
+
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(i, j) ((i) + (j))
+
+#endif
+
